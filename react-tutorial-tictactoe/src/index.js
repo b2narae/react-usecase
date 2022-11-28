ca749a3fd83ccab65ae9b0ae8606be616d1fa17f
b2narae <b2narae@gmail.com>
2022-11-28 11:37:49 +0900

    [ADD] Updating Status, Winner, HandleClick

--- react-tutorial-tictactoe/src/index.js
+++ react-tutorial-tictactoe/src/index.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const Square = ({value}) => {
+const Square = ({value, onClick}) => {
     return (
-        <button className='square'>
+        <button className='square' onClick={onClick}>
             {value}
         </button>
     )
 }
 
 const Board = () => {
-    const status = 'Next player: X';
+    const [xIsNext, setxIsNext] = useState(true);
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    let status;
 
+    const handleClick = (i) => {
+        const squaresCpy = squares.slice();
+
+        if (squaresCpy[i] || calculateWinner(squares)) {
+            return;
+        }
+
+        squaresCpy[i] = (xIsNext) ? 'X' : 'O';
+
+        setxIsNext(!xIsNext);
+        setSquares(squaresCpy);
+    }
+
+    const updateStatus = () => {        
+        const winner = calculateWinner(squares);
+
+        if (winner) {
+            status = 'Winner is ' + winner;
+        }
+        else {
+            status = 'Next player: ' + ((xIsNext) ? ('X') : ('O'));
+        }
+    }
+    
     const renderSquare = (i) => (
-        <Square value= {i}/>
+        <Square value={squares[i]} onClick={() => handleClick(i)}/>
+        // <Square value={squares[i]} onClick={handleClick(i)}/> // Too many re-render Error
     )
+
+    updateStatus();
 
     return (
         <div>
@@ -37,7 +66,6 @@ const Board = () => {
           </div>
         </div>
     )
-
 }
 
 const Game = () => {
@@ -54,8 +82,26 @@ const Game = () => {
     )
 }
 
+const calculateWinner = (squares) => {
+    const lines = [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    for (let i = 0; i < lines.length; ++i) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] == squares[b] && squares[b] == squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
-  
