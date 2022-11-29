2a5da27cfa41df0d90102ea47afe09ca47d8c8cb
b2narae <b2narae@gmail.com>
2022-11-29 21:00:04 +0900

    [ADD] Post Component

---
+++ react-blog/src/post/Post.jsx
@@ -0,0 +1,20 @@
+import './post.css'
+
+export default function Post() {
+  return (
+    <div className="post">
+        <img src="https://images.unsplash.com/photo-1509043759401-136742328bb3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80" alt="" className="postImg" />
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Music</span>
+                <span className="postCat">Life</span>
+            </div>
+            <span className="postTitle">
+                Lorem
+            </span>
+            <hr/>
+            <span className="postDate">1 hour ago</span>
+        </div>
+    </div>
+  )
+}
